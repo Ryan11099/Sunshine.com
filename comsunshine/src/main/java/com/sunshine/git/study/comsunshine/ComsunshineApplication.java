828a35edfe25a853a53af9bbf448b6b64d492a20
Ryan <l>
2020-04-28 15:46:46 +0800

    代码提交

---
+++ comsunshine/src/main/java/com/sunshine/git/study/comsunshine/ComsunshineApplication.java
@@ -0,0 +1,13 @@
+package com.sunshine.git.study.comsunshine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComsunshineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComsunshineApplication.class, args);
+    }
+
+}
---
+++ comsunshine/src/test/java/com/sunshine/git/study/comsunshine/ComsunshineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunshine.git.study.comsunshine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComsunshineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
